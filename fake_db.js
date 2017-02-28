b7f15b03b2f34b2e869d13a2bbed09aebe9a0646
DejonGill <dejonjgill@gmail.com>
2017-02-28 13:17:20 -0800

    update to seed data, dependencies, added babelrc. Completed basic webpack config, touched some files and updated gitignore to exclude bundle.js

--- fake_db.js
+++ fake_db.js
@@ -28,26 +28,30 @@ const dbData = {
           author: 5,
           comments: [
             { title: 'Cool', text: 'This would be easier with Sql!', author: 0, comments: [] }
-          ]
+          ],
+          created_at: '12:12::12'
         }
       ]
     },
     'Being a cowboy': {
       posts: [
-        title: 'There\'s nothing to eat',
-        text: 'Why is there never anything to eat on this ship?',
-        author: 1,
-        comments: [
-          {
-            title: '3 Easy Steps to Getting Food',
-            text: '1. Find a bounty head. 2. Capture that bounty head. 3. Get the reward. Bonus Step! Stop complaining on the Bebop!',
-            author: 2,
-            comments: [
-              { title: 'Jet you are boring!', text: 'And you\'re always so grumpy!', author: 3, comments: [] },
-              { title: 'Woof!', text: 'Woof Woof!', author: 4, comments: [] }
-            ]
-          }
-        ]
+        {
+          title: 'There\'s nothing to eat',
+          text: 'Why is there never anything to eat on this ship?',
+          author: 1,
+          comments: [
+            {
+              title: '3 Easy Steps to Getting Food',
+              text: '1. Find a bounty head. 2. Capture that bounty head. 3. Get the reward. Bonus Step! Stop complaining on the Bebop!',
+              author: 2,
+              comments: [
+                { title: 'Jet you are boring!', text: 'And you\'re always so grumpy!', author: 3, comments: [], created_at: '13:00:00' },
+                { title: 'Woof!', text: 'Woof Woof!', author: 4, comments: [], created_at: '12:59:00' }
+              ]
+            }
+          ],
+          created_at: '12:13:12'
+        }
       ]
     }
   },
