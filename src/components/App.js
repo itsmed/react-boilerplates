a839566bce968ec6ce3691fc44fddd30c48a1d81
DejonGill <dejonjgill@gmail.com>
2017-02-28 18:28:52 -0800

    Added state

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
+import dbData from '../../fake_db';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: dbData
+    };
+  }
+
   render() {
+    console.log('state', this.state);
     return <div>
       <h1>Hello from the app!</h1>
       <p>This was more difficult than webpack 1!</p>
       <p>But it works now!</p>
-      <h3>Bitches</h3>
+      <pre>
+        <code>
+          { 12 * 12}
+        </code>
+      </pre>
     </div>
   }
 }
