e1df7322774d81299e352d95490722727ae55982
DejonGill <dejonjgill@gmail.com>
2017-02-28 16:20:55 -0800

    webpack dev server hot reloading enables

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div>
+      <h1>Hello from the app!</h1>
+      <p>This was more difficult than webpack 1!</p>
+      <p>But it works now!</p>
+    </div>
+  }
+}
+
+export default App;
