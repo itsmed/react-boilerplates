3cdaf7a1b5d9e2e5cfd2a7a78b57b00565a32397
DejonGill <dejonjgill@gmail.com>
2017-03-02 16:55:30 -0800

    This is making me miss redux

--- src/components/categoriesContainer/PostCategoryPreview.js
+++ src/components/categoriesContainer/PostCategoryPreview.js
@@ -6,11 +6,12 @@ class PostCategoryPreview extends Component {
   }
 
   render() {
-    const { title, author, text } = this.props;
+    const { title, author, text } = this.props.post;
     return <div>
       <h1>{ title }</h1>
       <h3>{ author }</h3>
-      <p>{ text.split(' ').slice(0, 10) }</p>
+      <p>{ text.split(' ').slice(0, 10).join(' ') }</p>
+      {this.props.post.comments.length > 1 ? this.props.post.comments.length + ' comments' : this.props.post.comments.length + ' comment'}
     </div>;
   }
 }
