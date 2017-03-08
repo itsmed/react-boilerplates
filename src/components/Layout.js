071388e7f7bce69f1ffb4e8dbec3f8ad14cb677d
DejonGill <dejonjgill@gmail.com>
2017-03-07 20:27:20 -0800

    Big trouble in little react router town. Need internet for research, I thought I had this right

---
+++ src/components/Layout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Landing = (props) => (
+  <div>
+  { props.children }
+  </div>
+)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
 import ClientApp from './components/App';
 
-const App = () => (
-  <div>
-    <App />
-  </div>
-);
-
-render(<ClientApp />,
-  document.getElementById('app'));
+render(<Router history={browserHistory} routes={routes}>
+  <ClientApp />
+</Router>, document.getElementById('app'));
